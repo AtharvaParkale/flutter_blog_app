15b25f8e17d6113196e19f4a380eb4dd12b0972d
Atharva Parkale <xatharvaparkale@freo.money>
2024-10-06 01:11:55 +0530

    Initial commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://bvypjieernngoaweanyc.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2eXBqaWVlcm5uZ29hd2VhbnljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgxMzQ1OTcsImV4cCI6MjA0MzcxMDU5N30.1xfoN2MvhfM3WefG3gCRAUmyXKmccakxB2cQ7EY4P68';
+}
